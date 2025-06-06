3adc958ad37a911e3b8bb82c43d6f4bf70b02556
Démontant <olivier@creativconflans.fr>
2025-06-06 13:52:38 +0200

    feat: ajout de la bibliothèque astro-icon pour intégrer des icônes, mise à jour des composants pour utiliser des icônes dynamiques, amélioration de la cohérence des styles avec l'utilisation de la couleur "cryan" pour les textes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,21 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   site: 'https://zestcrm.fr',
-  integrations: [
-    tailwind({
-      // Configuration Tailwind personnalisée
-      config: {
-        applyBaseStyles: false, // On gère nous-mêmes les styles de base
-      },
-    }),
-    sitemap(),
-    partytown({
-      // Pour optimiser Google Analytics
-      config: {
-        forward: ['gtag'],
-      },
-    }),
-  ],
+  integrations: [tailwind({
+    // Configuration Tailwind personnalisée
+    config: {
+      applyBaseStyles: false, // On gère nous-mêmes les styles de base
+    },
+  }), sitemap(), partytown({
+    // Pour optimiser Google Analytics
+    config: {
+      forward: ['gtag'],
+    },
+  }), icon()],
   output: 'static', // Site statique pour de meilleures performances
   compressHTML: true,
   build: {
